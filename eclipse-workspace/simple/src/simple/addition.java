3a22de2935509b0a31b2c6e20ace1f8ce9116f0f
HPWorld <HPWorld@anuja>
2022-02-18 22:00:39 +0530

    sample programs

---
+++ eclipse-workspace/simple/src/simple/addition.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class addition {
+
+	public static void main(String[] args) {
+		int num1=2,num2=3,sum;
+		sum=num1+num2;
+		System.out.println("addition of two numbers is" +sum);
+
+	}
+
+}
---
+++ eclipse-workspace/simple/src/simple/addition2.java
@@ -0,0 +1,21 @@
+package simple;
+
+import java.util.Scanner;
+
+public class addition2 {
+
+	public static void main(String[] args) {
+		int x,y,sum;
+		Scanner in =new Scanner(System.in);
+		System.out.println("emter first number");
+		x=in.nextInt();
+		System.out.print("enter second number");
+		y=in.nextInt();
+		sum=x+y;
+		System.out.println("sum of two numbers is" +sum);
+		
+		
+
+	}
+
+}
