06c5ef6f3af93a3abdf272282d6fd967f43d9e1f
HPWorld <HPWorld@anuja>
2022-02-24 22:14:07 +0530

    ScannerPractice programs

---
+++ eclipse-workspace/abstractpractice/src/abstractpractice/scaner.java
@@ -0,0 +1,25 @@
+package abstractpractice;
+
+	import java.util.Scanner;
+
+	public class scaner {
+		 public static void main(String[] args) {
+
+			    // creates an object of Scanner
+			    Scanner input = new Scanner(System.in);
+
+			    System.out.print("Enter your name: ");
+
+			    // takes input from the keyboard
+			    String name = input.nextLine();
+
+			    // prints the name
+			    System.out.println("My name is " + name);
+
+			    // closes the scanner
+			    input.close();
+			  }
+			}
+
+
+
---
+++ eclipse-workspace/abstractpractice/src/abstractpractice/scanner2.java
@@ -0,0 +1,23 @@
+package abstractpractice;
+import java.util.Scanner;
+
+public class scanner2 {
+	
+	class Main {
+	  public static void main(String[] args) {
+
+	    // creates a Scanner object
+	    Scanner input = new Scanner(System.in);
+
+	    System.out.println("Enter an integer: ");
+
+	    // reads an int value
+	    int data1 = input.nextInt();
+
+	    System.out.println("Using nextInt(): " + data1);
+
+	    input.close();
+	  }
+	}
+
+}
