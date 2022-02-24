dd5350af8164965982523911c4cffdcbbfeb7ed3
HPWorld <HPWorld@anuja>
2022-02-24 22:26:52 +0530

    access modifiers examples

---
+++ eclipse-workspace/accessmodifiers1/src/accessmodifiers1/access.java
@@ -0,0 +1,24 @@
+package accessmodifiers1;
+
+	class Data {
+	    private String name;
+
+	    // getter method
+	    public String getName() {
+	        return this.name;
+	    }
+	    // setter method
+	    public void setName(String name) {
+	        this.name= name;
+	    }
+	}
+	public class access {
+	    public static void main(String[] main){
+	        Data d = new Data();
+
+	        // access the private variable using the getter and setter
+	        d.setName("Programiz");
+	        System.out.println(d.getName());
+	    }
+	}
+
